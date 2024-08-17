e51d2003d4be5f27a16a1a64004f8ebfb85048fd
BrennonMeireles <brennonmeireles7@gmail.com>
2024-08-17 16:44:21 -0300

    feat: created header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,16 @@
+import './style.css'
+
+export default function Header(){
+    return(
+        <header>
+            <h1>Logo</h1>
+            <ul>
+                <li>Link One</li>
+                <li>Link Two</li>
+                <li>Link Three</li>
+                <li>Link Four</li>
+            </ul>
+            <button>Try it for free</button>
+        </header>
+    )
+}
