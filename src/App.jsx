15189d1b16aa19154a571f0993079c373db68e6a
BrennonMeireles <brennonmeireles7@gmail.com>
2024-08-17 16:42:23 -0300

    feat: created default config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/header';
+import './App.css'
+import SectionHome from './components/section-home';
+
+function App() {
+  return (  
+    <>
+      <Header />
+      <SectionHome />
+    </>
+  )
+}
+
+export default App;
